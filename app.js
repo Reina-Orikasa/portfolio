e4172497984fa0e2b013868cd5beead7f16e02bb
Reina-Orikasa <tammichael76@gmail.com>
2020-11-28 00:15:17 -0800

    update site with icons and footer

--- app.js
+++ app.js
@@ -4,12 +4,25 @@ let todaySelector = document.querySelector('#today');
 let options = {
   month: 'long',
   day: '2-digit',
-  hour: 'numeric',
-  minute: '2-digit',
+  year: 'numeric-',
 };
 
-let formatDate = new Intl.DateTimeFormat('en', options);
+function displayDate(d, options, htmlSelector) {
+  let formatDate = new Intl.DateTimeFormat('en', options);
 
-let today = formatDate.format(d);
+  let today = formatDate.format(d);
 
-todaySelector.insertAdjacentHTML('beforeend', today);
+  htmlSelector.insertAdjacentHTML('beforeend', today);
+}
+
+displayDate(
+  d,
+  {
+    month: 'long',
+    day: '2-digit',
+    year: 'numeric',
+  },
+  todaySelector
+);
+
+displayDate(d, { year: 'numeric' }, document.querySelector('.year'));
