eaf4705b221532a2355b47d30e3383ed8dacea65
Reina-Orikasa <tammichael76@gmail.com>
2022-09-02 23:15:13 +0900

    update box colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'kale-blue': '#00c9ff',
-        'kale-green': '#92fe9d',
-        'lily-left': '#efd5ff',
-        'lily-right': '#515ada',
-        'deep-blue': '#000851',
-        'ocean-blue': '#1CB5E0',
-        'fairy-pink': '#EE99AC',
-        'pokemon-electric': '#F8D030',
-        'pokemon-water': '#6890F0',
-        'compliment': '#515ada',
+        compliment: '#7ca5ff',
+        darkCompliment: '#ffb37c',
       },
     },
   },
