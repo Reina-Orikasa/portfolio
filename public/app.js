7feb971105d9295d6d9b0a01b0537b7b05919eb5
Reina-Orikasa <tammichael76@gmail.com>
2022-09-04 00:21:25 +0900

    add new section

--- public/app.js
+++ public/app.js
@@ -1,31 +1,55 @@
 gsap.registerPlugin(ScrollTrigger);
 
-let d = new Date();
+// let d = new Date();
 
-let options = {
-  month: 'long',
-  day: '2-digit',
-  year: 'numeric-',
-};
+// let options = {
+//   month: 'long',
+//   day: '2-digit',
+//   year: 'numeric-',
+// };
 
-function displayDate(d, options, htmlSelector) {
-  let formatDate = new Intl.DateTimeFormat('en', options);
+// function displayDate(d, options, htmlSelector) {
+//   let formatDate = new Intl.DateTimeFormat('en', options);
 
-  let today = formatDate.format(d);
+//   let today = formatDate.format(d);
 
-  htmlSelector.insertAdjacentHTML('beforeend', today);
-}
+//   htmlSelector.insertAdjacentHTML('beforeend', today);
+// }
 
-displayDate(
-  d,
-  {
-    year: 'numeric',
-  },
-  document.querySelector('.year')
-);
+// displayDate(
+//   d,
+//   {
+//     year: 'numeric',
+//   },
+//   document.querySelector('.year')
+// );
 
 gsap.from('#header-text', {
   duration: 0.8,
   y: -300,
   ease: 'bounce',
+});
+
+gsap.to('.created', {
+  scrollTrigger: '.created',
+  duration: 0.3,
+  y: -100,
+  delay: 0.4,
+  opacity: 1,
+});
+
+gsap.to('.created1', {
+  scrollTrigger: '.bg-3',
+  duration: 0.3,
+  y: -100,
+  delay: 0.4,
+  opacity: 1,
+});
+
+gsap.to('.created2', {
+  scrollTrigger: '.bg-4',
+  duration: 0.3,
+  y: -100,
+  delay: 0.4,
+  opacity: 1,
 });
